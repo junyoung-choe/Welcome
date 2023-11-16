6bddf0b84a14d760f9c5261af8d7d15bffe293c9
rcpcmap <soohwan4030@gmail.com>
2023-11-17 00:01:48 +0900

    add board function without css

--- src/components/board/BoardList.vue
+++ src/components/board/BoardList.vue
@@ -1,10 +1,19 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
-import { listArticle } from '@/api/board.js';
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { listArticle } from "@/api/board.js";
 
-import BoardListItem from '@/components/board/item/BoardListItem.vue';
+import VSelect from "@/components/common/VSelect.vue";
+import BoardListItem from "@/components/board/item/BoardListItem.vue";
+import VPageNavigation from "@/components/common/VPageNavigation.vue";
 const router = useRouter();
+
+const selectOption = ref([
+  { text: "검색조건", value: "" },
+  { text: "글번호", value: "board_id" },
+  { text: "제목", value: "board_title" },
+  { text: "작성자아이디", value: "user_id" },
+]);
 
 const boards = ref([]);
 const currentPage = ref(1);
@@ -13,16 +22,21 @@ const { VITE_ARTICLE_LIST_SIZE } = import.meta.env;
 const param = ref({
   pgno: currentPage.value,
   spp: VITE_ARTICLE_LIST_SIZE,
-  key: '',
-  word: '',
+  key: "",
+  word: "",
 });
 
 onMounted(() => {
   getArticleList();
 });
 
+const changeKey = (val) => {
+  console.log("BoarList에서 선택한 조건 : " + val);
+  param.value.key = val;
+};
+
 const getArticleList = () => {
-  console.log('서버에서 글목록 얻어오자!!!', param.value);
+  console.log("서버에서 글목록 얻어오자!!!", param.value);
   listArticle(
     param.value,
     ({ data }) => {
@@ -35,12 +49,57 @@ const getArticleList = () => {
     }
   );
 };
+
+const onPageChange = (val) => {
+  console.log(val + "번 페이지로 이동 준비 끝!!!");
+  currentPage.value = val;
+  param.value.pgno = val;
+  getArticleList();
+};
+
+const moveWrite = () => {
+  router.push({ name: "board-write" });
+};
 </script>
 
 <template>
   <div>
-    <BoardListItem v-for="board in boards" :key="board.board_id" :board="board" />
+    <button
+      type="button"
+      class="btn btn-outline-primary btn-sm"
+      @click="moveWrite"
+    >
+      글쓰기
+    </button>
   </div>
+  <div class="col-md-5 offset-5">
+    <form class="d-flex">
+      <VSelect :selectOption="selectOption" @onKeySelect="changeKey" />
+      <div class="input-group input-group-sm ms-1">
+        <input
+          type="text"
+          class="form-control"
+          v-model="param.word"
+          placeholder="검색어..."
+        />
+        <button class="btn btn-dark" type="button" @click="getArticleList">
+          검색
+        </button>
+      </div>
+    </form>
+  </div>
+  <div>
+    <BoardListItem
+      v-for="board in boards"
+      :key="board.board_id"
+      :board="board"
+    />
+  </div>
+  <VPageNavigation
+    :current-page="currentPage"
+    :total-page="totalPage"
+    @pageChange="onPageChange"
+  ></VPageNavigation>
 </template>
 
 <style scoped></style>
--- src/components/board/BoardWrite.vue
+++ src/components/board/BoardWrite.vue
@@ -1,7 +1,22 @@
-<script setup></script>
+<script setup>
+import BoardFormItem from "./item/BoardFormItem.vue";
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-10">
+          <h2 class="my-3 py-3 shadow-sm bg-light text-center">
+            <mark class="sky">글쓰기</mark>
+          </h2>
+        </div>
+        <div class="col-lg-10 text-start">
+          <BoardFormItem type="regist" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
