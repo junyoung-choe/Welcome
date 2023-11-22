894bba5a1f20a39627a123ae8307106f76299050
junyoung-choe <wjdghkswnsdu@naver.com>
2023-11-22 22:12:40 +0900

    feat: use DataTimeFormat in tourboard

--- src/main/java/com/welcome/enjoytrip/tourboard/model/TourBoardDto.java
+++ src/main/java/com/welcome/enjoytrip/tourboard/model/TourBoardDto.java
@@ -1,6 +1,7 @@
 package com.welcome.enjoytrip.tourboard.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,9 +16,13 @@ public class TourBoardDto {
     private long user_id;
     private String tourboard_tourName;
     private String tourboard_keyword;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime tourboard_regDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime tourboard_startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime tourboard_endDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime tourboard_deadLineDate;
     private int tourboard_price;
     private int tourboard_salePrice;
