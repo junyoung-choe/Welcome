bfc359b96358a3a6a23c0bd57df903de7c40dcde
ksh4030 <kei7424030@gmail.com>
2023-11-23 10:41:09 +0900

    feat: board-css

--- src/components/board/BoardView.vue
+++ src/components/board/BoardView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { ref, onMounted } from "vue";
-import { useRoute, useRouter } from "vue-router";
-import { detailArticle, deleteArticle } from "@/api/board";
-import { jwtDecode } from "jwt-decode";
-import { localAxios } from "@/util/http-commons";
+import { ref, onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { detailArticle, deleteArticle } from '@/api/board';
+import { jwtDecode } from 'jwt-decode';
+import { localAxios } from '@/util/http-commons';
 
 const route = useRoute();
 const router = useRouter();
@@ -14,18 +14,19 @@ const board = ref({});
 const comment = ref({
   user_id: 0,
   board_id: 0,
-  comment_content: "댓글",
+  comment_content: '',
 });
 
 // 로그인한 회원과 토큰에 저장된 회원이 같을때만 삭제 수정 버튼이 생성된다
 const possible = ref(false);
 
 const commentsPossible = ref(false);
+const date = ref('');
 
 const getUserInformation = () => {
-  let token = sessionStorage.getItem("accessToken");
+  let token = sessionStorage.getItem('accessToken');
   let decodeToken = jwtDecode(token);
-  console.log(decodeToken.userId + " , " + board.value);
+  console.log(decodeToken.userId + ' , ' + board.value);
   if (decodeToken.userId == board.value.user_id) {
     possible.value = true;
   }
@@ -44,13 +45,14 @@ onMounted(() => {
 
 const getBoard = () => {
   // const { articleno } = route.params;
-  console.log(board_id + "번글 얻으러 가자!!!");
+  console.log(board_id + '번글 얻으러 가자!!!');
   detailArticle(
     board_id,
     ({ data }) => {
       // console.log("1");
       board.value = data;
       getUserInformation();
+      date.value = `${board.value.board_regDate[0]}/${board.value.board_regDate[1]}/${board.value.board_regDate[2]} ${board.value.board_regDate[3]}:${board.value.board_regDate[4]}`;
     },
     (error) => {
       console.log(error);
@@ -59,16 +61,16 @@ const getBoard = () => {
 };
 
 function moveList() {
-  router.push({ name: "board-list" });
+  router.push({ name: 'board-list' });
 }
 
 function moveModify() {
-  router.push({ name: "board-modify", params: { board_id } });
+  router.push({ name: 'board-modify', params: { board_id } });
 }
 
 function onDeleteArticle() {
   // const { articleno } = route.params;
-  console.log(board_id + "번글 삭제하러 가자!!!");
+  console.log(board_id + '번글 삭제하러 가자!!!');
   deleteArticle(
     board_id,
     (response) => {
@@ -85,39 +87,31 @@ function commentRegButton() {
 }
 
 function regComment(param, success, fail) {
-  console.log("해당 글에 댓글 등록하러 갑니데이");
+  console.log('해당 글에 댓글 등록하러 갑니데이');
   local.post(`board/comment`, param).then(success).catch(fail);
 }
 </script>
 
 <template>
-  <!-- <div>{{ board }}</div> -->
-  <div>
-    제목 :
-    {{ board.board_title }}
-  </div>
-  <div>
-    내용 :
-    {{ board.board_content }}
-  </div>
-  <div>
-    작성자 :
-    {{ board.user_name }}
-  </div>
-  <div>
-    작성일자 :
-    {{ board.board_regDate }}
+  <div class="main-content">
+    <div class="title">
+      {{ board.board_title }}
+    </div>
+    <div class="dateANDname">
+      <div class="userName">
+        {{ board.user_name }}
+      </div>
+      <div class="date">
+        {{ date }}
+      </div>
+    </div>
+    <div class="content">
+      {{ board.board_content }}
+    </div>
   </div>
   <div class="d-flex justify-content-end">
-    <button type="button" class="btn btn-outline-primary mb-3" @click="moveList">글목록</button>
-    <button
-      v-if="possible"
-      type="button"
-      class="btn btn-outline-success mb-3 ms-1"
-      @click="moveModify"
-    >
-      글수정
-    </button>
+    <button type="button" class="list-btn" @click="moveList">글목록</button>
+    <button v-if="possible" type="button" class="list-btn" @click="moveModify">글수정</button>
     <button
       v-if="possible"
       type="button"
@@ -130,36 +124,84 @@ function regComment(param, success, fail) {
 
   <hr />
   <!--  댓글 부분   -->
-  <input
-    v-if="commentsPossible"
-    type="text"
-    placeholder="댓글내용..."
-    v-model="comment.comment_content"
-  />
-  <button
-    v-if="commentsPossible"
-    type="button"
-    class="btn btn-outline-danger mb-3 ms-1"
-    @click="commentRegButton"
-  >
-    댓글작성
-  </button>
+  <div class="comment-input-box">
+    <input
+      v-if="commentsPossible"
+      type="text"
+      class="comment-input"
+      placeholder="댓글내용..."
+      v-model="comment.comment_content"
+    />
+    <button v-if="commentsPossible" type="button" class="comment-add-btn" @click="commentRegButton">
+      댓글작성
+    </button>
+  </div>
   <!-- <div>{{ board.commentDtos }}</div> -->
-  <br />
-  <br />
-  <template v-for="(comments, index) in board.commentDtos" :key="comments.comment_id">
-    {{ index + 1 }}
-    댓글내용
-    {{ comments.comment_content }}
-    작성시간
-    {{ comments.comment_regDate }}
-    <br />
-    <br />
-  </template>
+  <div class="comment-box">
+    <template v-for="(comments, index) in board.commentDtos" :key="comments.comment_id">
+      <div class="comment-each">
+        {{ comments.comment_content }}
+      </div>
+    </template>
+  </div>
   <!-- {{ comment }} -->
   <!-- <template v-for="index in board.commentDtos.length">
     {{ board.commentDtos[index - 1].comment_id }}
   </template> -->
 </template>
 
-<style scoped></style>
+<style scoped>
+.title {
+  font-size: 35px;
+  font-weight: 700;
+}
+.dateANDname {
+  display: flex;
+}
+.userName {
+  font-size: 13px;
+  margin-right: 10px;
+}
+.date {
+  font-size: 13px;
+  color: rgba(0, 0, 0, 0.6);
+}
+.content {
+  margin-top: 20px;
+  font-size: 18px;
+  margin-bottom: 20px;
+}
+.list-btn {
+  border: none;
+  background-color: white;
+  color: rgba(0, 0, 0, 0.7);
+}
+.comment-input-box {
+  display: flex;
+}
+.comment-input {
+  width: 500px;
+  border-radius: 10px;
+  border: 1px solid rgba(0, 0, 0, 0.4);
+  padding: 5px;
+  height: 40px;
+}
+.comment-add-btn {
+  border: 1px solid rgba(0, 0, 0, 0.4);
+  background-color: white;
+  margin-left: 20px;
+  border-radius: 10px;
+}
+
+.comment-add-btn:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+
+.comment-box {
+  margin-top: 30px;
+}
+.comment-each {
+  margin-bottom: 10px;
+  margin-left: 10px;
+}
+</style>
