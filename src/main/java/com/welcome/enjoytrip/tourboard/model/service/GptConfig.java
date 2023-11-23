83356fd6ee8122a4b3a1c9ab3b165b6b99c07abd
rcpcmap <soohwan4030@gmail.com>
2023-11-23 23:12:28 +0900

    hide key

--- src/main/java/com/welcome/enjoytrip/tourboard/model/service/GptConfig.java
+++ src/main/java/com/welcome/enjoytrip/tourboard/model/service/GptConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 public class GptConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "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";
+    public static final String API_KEY = "";
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKEN = 300;
     public static final Double TEMPERATURE = 0.0;
